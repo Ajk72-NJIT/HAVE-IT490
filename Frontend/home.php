1e0a42c9679b35131d60ab63d2ae30980403dab5
Edwin Leon <ecl24@njit.edu>
2024-03-18 16:04:55 -0400

    Updated home/landing files, left out from last commit

--- Frontend/home.php
+++ Frontend/home.php
@@ -1,21 +1,30 @@
 <html>
-<h1>login page</h1>
+<head>
+    <title>Login Page</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
 <body>
-
-<form action="testRabbitMQClient.php" method="POST">
-	username:<input type="text" id='username' name='username'><br>
-	password: <input type="password" id='password' name='password'><br>
-	<button type='submit' name="login">Login</button>
-</form>
-
-<h4> or register: </h4>
-
-<form action="testRabbitMQClient.php" method="POST">
-	username:<input type="text" id='username' name='username'><br>
-	password: <input type="text" id='password' name='password'><br>
-	confirm password: <input type="text" id='cpassword' name='cpassword'><br>
-	email: <input type="text" id='email' name='email'><br>
-	<button type='submit' name="register">Register</button>
-</form>
+    <div class="container">
+        <h1 class="title">Digital Fridge Login</h1>
+        <form action="testRabbitMQClient.php" method="POST" class="login-form">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" class="input-field">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" class="input-field">
+            <button type="submit" name="login" class="submit-button">Login</button>
+        </form>
+        <h4>Don't have an account? Register!</h4>
+        <form action="testRabbitMQClient.php" method="POST" class="register-form">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" class="input-field">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" class="input-field">
+            <label for="cpassword">Confirm Password</label>
+            <input type="password" id="cpassword" name="cpassword" class="input-field">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" class="input-field">
+            <button type="submit" name="register" class="submit-button">Register</button>
+        </form>
+    </div>
 </body>
 </html>
--- Frontend/landing.php
+++ Frontend/landing.php
@@ -1,4 +1,4 @@
-<?php
+<?php /*
 session_start();
 require_once('path.inc');
 require_once('get_host_info.inc');
@@ -14,21 +14,76 @@ $request['message'] = 'Authorizing Token';
 $response = $client->send_request($request);
 if ($response['message'] != "authed"){
 	header('Location: home.php');
-}
+} */
 ?>
 
 <html>
-<h1>Landing</h1>
+<head>
+    <title>Landing Page</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
 <body>
+    <div class="container">
+        <h1 class="title">Your Digital Fridge</h1>
+        <p class="welcome-message">Welcome <?php echo $_SESSION['name']; ?>, what's in your Fridge?</p>
+        <div class="input-group">
+            <div class="dropdown">
+                <input type="text" id="ingredientInput" class="input-field" oninput="filterIngredients()" placeholder="Search for ingredients...">
+                <!-- Placeholder options, will eventually add to "Fridge" table -->
+				<div id="ingredientDropdown" class="dropdown-content">
+                    <a href="#" onclick="fillInput('Option A')">Option A</a>
+                    <a href="#" onclick="fillInput('Option B')">Option B</a>
+                    <a href="#" onclick="fillInput('Option C')">Option C</a>
+                    <a href="#" onclick="fillInput('Option D')">Option D</a>
+                    <a href="#" onclick="fillInput('Option E')">Option E</a>
+                    <a href="#" onclick="fillInput('Option F')">Option F</a>
+                    <a href="#" onclick="fillInput('Option G')">Option G</a>
+                    <a href="#" onclick="fillInput('Option H')">Option H</a>
+                    <a href="#" onclick="fillInput('Option I')">Option I</a>
+                    <a href="#" onclick="fillInput('Option J')">Option J</a>
+                </div>
+            </div>
+            <input type="number" id="quantity" name="quantity" class="input-field" placeholder="QTY" min="0" max="99">
+        </div>
+        <form action="testRabbitMQClient.php" method="POST">
+			<button type="submit" class="submit-button">Add to Fridge</button>
+            <button type="submit" name="logout" class="logout-button">Logout</button>
+        </form>
+    </div>
+    <script>
+        function filterIngredients() {
+            var input, filter, dropdown, options, i, txtValue;
+            input = document.getElementById("ingredientInput");
+            filter = input.value.toUpperCase();
+            dropdown = document.getElementById("ingredientDropdown");
+            options = dropdown.getElementsByTagName("a");
+            for (i = 0; i < options.length; i++) {
+                txtValue = options[i].textContent || options[i].innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    options[i].style.display = "";
+                } else {
+                    options[i].style.display = "none";
+                }
+            }
+            dropdown.classList.add("show");
+        }
 
-<?php
-echo 'welcome ';
-echo $_SESSION['name'];
-echo '!';
-?>
+        function fillInput(value) {
+            document.getElementById("ingredientInput").value = value;
+            document.getElementById("ingredientDropdown").classList.remove("show");
+        }
 
-<form action="testRabbitMQClient.php" method="POST">
-	<button type='submit' name="logout">logout</button>
-</form>
+        window.onclick = function(event) {
+            if (!event.target.matches('.input-field')) {
+                var dropdowns = document.getElementsByClassName("dropdown-content");
+                for (var i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
+    </script>
 </body>
 </html>
