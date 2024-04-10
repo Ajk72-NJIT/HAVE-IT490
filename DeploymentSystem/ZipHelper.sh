a1a4b729e0bb765bb81f832651c37ef27071e371
hdn <hdn@njit.edu>
2024-04-09 23:52:05 -0400

    folder structure

--- DeploymentSystem/ZipHelper.sh
+++ DeploymentSystem/ZipHelper.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-VMS=("10.211.55.5") #separate by space
+VMS=("DMZ:10.211.55.5") #separate by space
 USER="parallels"
 COPYFROM="/home/parallels/CopyFrom"
 COPYTO="/home/parallels/CopyTo"
 
 mkdir -p "$COPYTO"
-
+cd "$COPYTO"
 #Loop through hosts to find and copy files
 for HOST in "${VMS[@]}"; do
+
     echo "Connecting to $HOST"
     
+    SUBDIR=${HOST:0:3}
+    IP=${HOST#*:}
+    
+    mkdir -p "$COPYTO/$SUBDIR"
+    
     echo "Copying files from $HOST:$COPYFROM"
-    scp -r "$USER@$HOST:\"$COPYFROM\"/*" "$COPYTO/"
+    scp -r "$USER@$IP:$COPYFROM/"* "$COPYTO/$SUBDIR/"
 done
 
 VERSION=1
-while [[ -f "$COPYTO/HAVEFRIDGE v$VERSION.zip" ]]; do
+while [[ -f "HAVEFRIDGE v$VERSION.zip" ]]; do
     ((VERSION++))
 done
 ZIPNAME="HAVEFRIDGE v$VERSION.zip"
 
 #Zip files
-cd "$COPYTO"
+#cd "$COPYTO"
 
 echo "Zipping files..."
 zip -r "$ZIPNAME" .
