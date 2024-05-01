d7a32aa7b726f3c6957692ff454211773de7843e
hdn <hdn@njit.edu>
2024-05-01 00:27:54 -0400

    changes to copy

--- DeploymentSystem/my-rabbitmq-client/receive.php
+++ DeploymentSystem/my-rabbitmq-client/receive.php
@@ -11,6 +11,9 @@ echo " ... Waiting for messages\n";
 
 $callback = function($msg) {
   echo " ~ Received ", $msg->body, "\n";
+  $copyScript = '/home/parallels/HAVE-IT490/DeploymentSystem/DMZcopy.sh'
+  $output = shell_exec($copyScript);
+  echo "$output";
 };
 
 $channel->basic_consume('QA', '', false, true, false, false, $callback);
