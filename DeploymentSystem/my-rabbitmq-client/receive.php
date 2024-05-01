153fdc77cc19a2b6bf1e3add22da4ca06e9431ed
hdn <hdn@njit.edu>
2024-05-01 01:03:19 -0400

    rabbit working successfully

--- DeploymentSystem/my-rabbitmq-client/receive.php
+++ DeploymentSystem/my-rabbitmq-client/receive.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$connection = new AMQPStreamConnection('10.211.55.6', 5672, 'dmz', 'dmz');
 $channel = $connection->channel();
 
 $channel->queue_declare('QA', false, false, false, false);
@@ -11,7 +11,7 @@ echo " ... Waiting for messages\n";
 
 $callback = function($msg) {
   echo " ~ Received ", $msg->body, "\n";
-  $copyScript = '/home/parallels/HAVE-IT490/DeploymentSystem/DMZcopy.sh'
+  $copyScript = '/home/parallels/HAVE-IT490/DeploymentSystem/DMZcopy.sh';
   $output = shell_exec($copyScript);
   echo "$output";
 };
