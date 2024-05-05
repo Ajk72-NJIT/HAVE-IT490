e72be500da6fa50114b4a694ba6d05ac1557db2e
Edwin <ecl24@njit.edu>
2024-05-05 08:42:39 -0400

    Refined CSS, adjusted home.php to reflect, add img

--- Frontend/home.php
+++ Frontend/home.php
@@ -7,23 +7,23 @@
     <div class="container">
         <h1 class="title">Digital Fridge Login</h1>
         <form action="testRabbitMQClient.php" method="POST" class="login-form">
-            <label for="username">Username</label>
-            <input type="text" id="username" name="username" class="input-field">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" class="input-field">
-            <button type="submit" name="login" class="submit-button">Login</button>
+            <label for="login-username">Username</label>
+            <input type="text" id="login-username" name="username" class="input-field">
+            <label for="login-password">Password</label>
+            <input type="password" id="login-password" name="password" class="input-field">
+            <button type="submit" name="login">Login</button>
         </form>
         <h4>Don't have an account? Register!</h4>
         <form action="testRabbitMQClient.php" method="POST" class="register-form">
-            <label for="username">Username</label>
-            <input type="text" id="username" name="username" class="input-field">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" class="input-field">
-            <label for="cpassword">Confirm Password</label>
-            <input type="password" id="cpassword" name="cpassword" class="input-field">
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" class="input-field">
-            <button type="submit" name="register" class="submit-button">Register</button>
+            <label for="register-username">Username</label>
+            <input type="text" id="register-username" name="username" class="input-field">
+            <label for="register-password">Password</label>
+            <input type="password" id="register-password" name="password" class="input-field">
+            <label for="register-cpassword">Confirm Password</label>
+            <input type="password" id="register-cpassword" name="cpassword" class="input-field">
+            <label for="register-email">Email</label>
+            <input type="email" id="register-email" name="email" class="input-field">
+            <button type="submit" name="register">Register</button>
         </form>
     </div>
 </body>
