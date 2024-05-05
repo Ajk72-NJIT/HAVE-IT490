11eceda1f8538e849334d73c083e2ca8c3c093ba
root <root@IT490Database.myguest.virtualbox.org>
2024-05-05 15:19:11 -0400

    finished database and rabbit systemd

--- Database/systemd.sh
+++ Database/systemd.sh
@@ -4,8 +4,10 @@ sudo scp database.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
 
+sudo systemctl disable database
 sudo systemctl enable database
 
+sudo systemctl stop databse
 sudo systemctl start database
 
 sudo systemctl status database
