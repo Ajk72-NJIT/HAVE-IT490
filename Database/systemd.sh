6dc3e2ed51e344569c02db0ce8e2bf924c3877b2
Ajk72-NJIT <83480036+Ajk72-NJIT@users.noreply.github.com>
2024-05-05 14:56:25 -0400

    Update systemd.sh

--- Database/systemd.sh
+++ Database/systemd.sh
@@ -4,8 +4,8 @@ sudo scp database.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
 
-sudo systemctl enable dmz
+sudo systemctl enable database
 
-sudo systemctl start dmz
+sudo systemctl start database
 
-sudo systemctl status dmz
+sudo systemctl status database
